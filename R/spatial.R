58323dd036ee8ec3ab7831564f600d203c454f3d
rhijmans <r.hijmans@gmail.com>
2021-10-01 09:06:41 -0700

    for raster/terra dep switch

--- R/spatial.R
+++ R/spatial.R
@@ -9,7 +9,7 @@ function(object, weather, mstart, soilindex=NULL, soils=NULL, filename="", overw
 	if (watlim) {
 		stopifnot(inherits(soilindex, "SpatRaster"))
 		stopifnot(inherits(soils, "list"))
-		compareGeom(weather[1], soilindex, lyrs=FALSE)
+		terra::compareGeom(weather[1], soilindex, lyrs=FALSE)
 		needed <- c("tmin", "tmax", "srad", "prec", "vapr", "wind")
 		scol <- .makeSoilCollection(soils)
 	} else {
@@ -21,24 +21,24 @@ function(object, weather, mstart, soilindex=NULL, soils=NULL, filename="", overw
 		stop(paste("missing these variables:", needed[!(nms %in% needed)]))
 	}
 	weather <- weather[needed]
-	nss <- nlyr(weather)
+	nss <- terra::nlyr(weather)
 	if (!all(nss == nss[1])) stop("all subdatasets must have the same number of layers")
 	
 	#if (is.null(dates)) {
 		if (!weather$tmin@ptr$hasTime) {
 			stop("no dates supplied as argument or with tmin")
 		}
-		dates <- time(weather$tmin)
+		dates <- terra::time(weather$tmin)
 	#} else {
 		stopifnot(length(dates) == nrow(weather$tmin))
 	#}
 	if (any(is.na(dates))) {stop("NA in dates not allowed")}
-	out <- rast(weather, nlyr=length(mstart))
+	out <- terra::rast(weather, nlyr=length(mstart))
 	
 	use_raster <- FALSE
-	if (substr(unlist(sources(weather[1])[1]),1,6) == "NETCDF") {
+	if (substr(unlist(terra::sources(weather[1])[1]),1,6) == "NETCDF") {
 		use_raster <- TRUE
-		p <- unlist(sources(weather[1])[1])
+		p <- unlist(terra::sources(weather[1])[1])
 		f <- unlist(strsplit(gsub("NETCDF:\"", "", p), "\""))[1]
 		weather <- lapply(needed, function(i) raster::brick(f, varname=i))
 		names(weather) <- needed
